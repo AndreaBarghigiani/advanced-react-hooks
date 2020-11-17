e5d99c1086e1a7010032dd8bfdc5494ae7d75b08
Andrea Barghigiani <a.barghigiani@gmail.com>
2020-11-17 18:18:50 +0100

    01-extra-credit-done

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,15 +4,12 @@
 import * as React from 'react'
 
 function countReducer(state, newState) {
-	return newState;
+	return state + newState;
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-	// 💰 React.useReducer(countReducer, initialCount)
-  // const [count, setCount] = React.useState(initialCount)
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => setCount(count + step)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount);
+  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
