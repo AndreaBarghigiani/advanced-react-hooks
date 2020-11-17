2e84dbbf3a461a781e894aaf9534524b4e69fabb
Andrea Barghigiani <a.barghigiani@gmail.com>
2020-11-17 18:29:03 +0100

    01-extra-credit-2-done

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,12 +4,16 @@
 import * as React from 'react'
 
 function countReducer(state, newState) {
-	return state + newState;
+	return {
+		...state,
+		...newState
+	};
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, changeCount] = React.useReducer(countReducer, initialCount);
-  const increment = () => changeCount(step)
+	const [state, setState] = React.useReducer(countReducer, { count: initialCount } );
+	const {count} = state;
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
