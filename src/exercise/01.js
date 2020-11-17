b0fb819ec0031ad2a08bf00b389308c653255e84
Andrea Barghigiani <a.barghigiani@gmail.com>
2020-11-17 19:29:35 +0100

    01-extra-credit-4-done

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,21 @@
 
 import * as React from 'react'
 
-function countReducer(state, newState) {
-	return {
-		...state,
-		...newState
-	};
+function countReducer( state, action ){
+	switch (action.type) {
+		case "INCREMENT":
+			return { count: state.count + action.step }
+		default:
+			throw new Error(`Unsupported action type: ${action.type}`)
+	}
 }
 
 function Counter({initialCount = 0, step = 1}) {
-	const [state, setState] = React.useReducer(countReducer, { count: initialCount } );
-	const {count} = state;
-  const increment = () => setState({count: count + step})
+	const [state, dispatch] = React.useReducer(countReducer, {
+		count: initialCount,
+	})
+	const {count} = state
+	const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
